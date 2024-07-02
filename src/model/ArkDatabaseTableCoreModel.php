377de2c411bf4a104eca9c1d9df516661bd2889b
sinri <e.joshua.s.e@gmail.com>
2024-07-02 13:20:06 +0800

    3.0.5 fix bug about insert ignore

--- src/model/ArkDatabaseTableCoreModel.php
+++ src/model/ArkDatabaseTableCoreModel.php
@@ -26,7 +26,7 @@ abstract class ArkDatabaseTableCoreModel extends ArkDatabaseTableReaderModel
      */
     public function insertIgnoreOneRow(array $data, ?string $pk = null): ArkDatabaseQueryResult
     {
-        return $this->writeInto("INSERT INTO", $data, $pk);
+        return $this->writeInto("INSERT IGNORE", $data, $pk);
     }
 
     /**
